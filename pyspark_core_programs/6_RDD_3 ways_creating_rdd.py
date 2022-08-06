8e352108384b70a72a0ff23a01ec83be834ba9d2
vishwanath <jkashivishwanath5@gmail.com>
2022-08-06 14:22:46 +0530

    ways to create rdds-update comments

--- pyspark_core_programs/6_RDD_3 ways_creating_rdd.py
+++ pyspark_core_programs/6_RDD_3 ways_creating_rdd.py
@@ -13,6 +13,7 @@ a= [10,20,30,40,50]
 
 #1st way of creating rdd
 #create a data rdd with 2 partitions. 
+#each nodein cluster has 4 or x no of core processers. Each processor can take one partition.
 data = sparkContext.parallelize(a,2)
 dataOutput=data.collect()
 # Printing output.
@@ -28,7 +29,7 @@ text_file = sc.textFile("dbfs:/FileStore/shared_uploads/vissu4u4ever@gmail.com/2
 #text_file = sc.textFile("dbfs:/FileStore/shared_uploads/vissu4u4ever@gmail.com/2_EmployeeData-2.txt",n)
 
 
-#3rd way of creating rdd.
+#3rd way of creating rdd. here no of rdds will be same as no of partions when rdd got created in step1 (parallelize) or #stpe2(textFile).
 employee_data =text_file.map(lambda line: line.split(",")).map(lambda x: (x[3], int(x[2])))
 emp_output= employee_data.collect()
 # Printing output.
