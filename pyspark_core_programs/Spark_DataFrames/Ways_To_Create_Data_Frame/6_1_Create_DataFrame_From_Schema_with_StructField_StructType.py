df3b6be3001985e46f099659935c5a1544607871
vishwanath <jkashivishwanath5@gmail.com>
2022-10-03 22:20:50 +0530

    added data frames programs3

---
+++ pyspark_core_programs/Spark_DataFrames/Ways_To_Create_Data_Frame/6_1_Create_DataFrame_From_Schema_with_StructField_StructType.py
@@ -0,0 +1,30 @@
+# Databricks notebook source
+#https://www.geeksforgeeks.org/defining-dataframe-schema-with-structfield-and-structtype/
+
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType,StructField,FloatType,IntegerType,LongType,StringType
+
+spark = SparkSession.builder.master("local").appName("App1").getOrCreate()
+
+input_data = [("Refrigerator", 112345, 4.0, 12499),
+                  ("LED TV", 114567, 4.2, 49999),
+                  ("Washing Machine", 113465, 3.9, 69999),
+                  ("T-shirt", 124378, 4.1, 1999),
+                  ("Jeans", 126754, 3.7, 3999),
+                  ("Running Shoes", 134565, 4.7, 1499),
+                  ("Face Mask", 145234, 4.6, 999)]
+
+schema_info = StructType([ 
+                            StructField("Product Name",StringType(),True),
+                            StructField("Product ID",LongType(),True),
+                            StructField("Rating",FloatType(),True),
+                            StructField("Product Price",IntegerType(),True)
+                         ])
+df1=spark.createDataFrame(input_data,schema_info)
+df1.show()
+df1.printSchema()
+
+
+
+
+
